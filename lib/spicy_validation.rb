8bf22bd6f2fbe09f49b0f094f38bfbab54e0c855
Naoto Ono <nono19@students.desu.edu>
2021-02-26 23:47:01 +0900

    initialize repository

---
+++ lib/spicy_validation.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "spicy_validation/version"
+
+module SpicyValidation
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/spicy_validation/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SpicyValidation
+  VERSION = "0.1.0"
+end
