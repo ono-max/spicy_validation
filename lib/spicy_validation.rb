fdf8201d68ecbba91b235a995df76e6c83e31b2c
Naoto Ono <nono19@students.desu.edu>
2021-04-04 20:42:21 +0900

    modify spicy_validation.rb -require files

--- lib/spicy_validation.rb
+++ lib/spicy_validation.rb
@@ -3,3 +3,7 @@
 require "rails"
 require_relative "spicy_validation/version"
 require_relative "spicy_validation/railtie"
+require_relative "spicy_validation/renderer"
+require_relative "spicy_validation/schema"
+require_relative "spicy_validation/validation"
+require_relative "spicy_validation/monkey/new_hash_syntax"
